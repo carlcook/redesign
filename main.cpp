5c91eff4fa6bdc3b4558abf9df263695a4823572
Carl Cook <carl.cook@gmail.com>
2015-06-23 00:28:36 +0200

    More WIP

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <cassert>
+
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 using namespace std;
 using Callback = std::function<void(void)>;
@@ -27,24 +35,72 @@ public:
     mCallbacks.push_back(callback);
   }
 
-  void RegisterPipeListener(const std::string queueName, PipeListener& listener)
+  std::pair<int, int> RegisterPipeListener(const std::string queueName, PipeListener& listener)
   {
-    // TODO open fifo pipe and use the given fd
-    mPipeListeners[0] = &listener;
+    if (mkfifo(queueName.c_str(), S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH) == -1)
+    {
+      if (errno != EEXIST)
+        throw std::runtime_error(strerror(errno));
+    }
+    int readFd;
+    if ((readFd = open(queueName.c_str(), O_RDONLY | O_NONBLOCK)) < 0)
+      throw std::runtime_error(strerror(errno));
+    mPipeListeners[readFd] = &listener;
+    int writeFd;
+    if ((writeFd = open(queueName.c_str(), O_WRONLY | O_NONBLOCK)) < 0)
+      throw std::runtime_error(strerror(errno));
+    return std::make_pair(readFd, writeFd);
   }
 
   void Start()
   {
     while (true)
     {
-      // listen to pipes for data
-      // if it comes, call correct listener
+      for (auto& kv : mPipeListeners)
+      {
+        char buf[2];
+
+        // Form descriptor
+        fd_set readset;
+        struct timeval tv;
+        tv.tv_sec = 0;
+        tv.tv_usec = 100000;
+        FD_ZERO(&readset);
+        FD_SET(kv.first, &readset);
+        int available = select(kv.first + 1, &readset, NULL, NULL, &tv);
+        if (available == -1)
+          throw std::runtime_error("Select failed");
+        if (available > 0 && FD_ISSET(kv.first, &readset))
+        {
+          int n = read(kv.first, buf, sizeof(buf));
+          if (n == 2)
+          {
+            int command = atoi(buf);
+            switch (command)
+            {
+            case 0:
+              kv.second->OnData("0", 2);
+              break;
+            case 1:
+              kv.second->OnData("1", 2);
+              break;
+            case 2:
+              kv.second->OnData("2", 2);
+              break;
+            default:
+              throw std::runtime_error("Unknown pipe command");
+              break;
+            }
+          }
+        }
+      }
 
       // also clear callback queue
       for (auto& callback : mCallbacks)
       {
         callback();
       }
+      mCallbacks.clear();
     }
   }
 };
@@ -59,29 +115,60 @@ struct OrderInsertMsg
 class ExecModule : private PipeListener
 {
 private:
-  EventEngine mEventEngine;
+  EventEngine& mEventEngine;
 
   // for now, only one callback at a time
   std::function<void(void)> mInsertCallback;
+  std::function<void(void)> mFillCallback;
+
+  int mWriteFd;
 public:
   ExecModule(EventEngine& eventEngine) :
     mEventEngine(eventEngine)
   {
-    // TODO create fifo pipe here
-    mEventEngine.RegisterPipeListener("ExecModule", *this);
+    mWriteFd = mEventEngine.RegisterPipeListener("/tmp/ExecModule", *this).second;
   }
 
-  void SendInsert(const OrderInsertMsg& orderInsertMsg, std::function<void(int)> callback)
+  ~ExecModule()
   {
-    // record the callback with the given order tag
-    mInsertCallback = std::bind(callback, orderInsertMsg.mTag);
-    // TODO write to the named pipe, simulating an exchange reply
+    if (mWriteFd >= 0)
+    {
+      close(mWriteFd);
+    }
+  }
+
+  void SendInsert(const OrderInsertMsg& orderInsertMsg, std::function<void(int)> replyCallback, std::function<void(int)> fillCallback)
+  {
+    std::cout << "Exec module: sending insert to market\n";
+
+    // record the callbacks with the given order tag
+    mInsertCallback = std::bind(replyCallback, orderInsertMsg.mTag);
+    mFillCallback = std::bind(fillCallback, orderInsertMsg.mTag);
+
+    // simulate exchange reply (will be picked up in next cycle)
+    if (write(mWriteFd, "1", 2) < 2)
+    {
+      throw std::runtime_error("Failed to write to named pipe");
+    }
   }
 
   void OnData(const char* data, size_t msgLen) override
   {
-    // TODO either an insert reply or a fill message
-    mEventEngine.Post(mInsertCallback);
+    assert(msgLen == 2);
+    int command = atoi(data);
+    switch (command)
+    {
+      case 1:
+        std::cout << "Exec module: received exchange reply, calling back sender\n";
+        mEventEngine.Post(mInsertCallback);
+        break;
+      case 2:
+        std::cout << "Exec module: received exchange fill, calling back sender\n";
+        mEventEngine.Post(mFillCallback);
+        break;
+      default:
+        throw std::runtime_error("Unknown pipe command");
+      }
   }
 };
 
@@ -95,7 +182,7 @@ private:
   {
     New,
     Inserted,
-    Deleted
+    Filled
   };
 
   struct Order
@@ -110,15 +197,30 @@ private:
 
   void HandleInsertOrder(const OrderInsertMsg& orderInsertMsg)
   {
+    std::cout << "Order manager: received order insert message\n";
     mOrders.insert(std::make_pair(orderInsertMsg.mTag, Order()));
+
+    // TODO now check price collars
+
+    auto fillCallback = [this](int tag)
+    {
+      std::cout << "Order manager: received fill\n";
+
+      // update order state again
+      auto& order = mOrders[tag];
+      order.mOrderState = OrderState::Filled;
+      // TODO slow thread work
+    };
     mExecModule.SendInsert(orderInsertMsg, [this](int tag)
     {
+        std::cout << "Order manager: received order reply\n";
+
         // update order state again now that we have been called back
         auto& order = mOrders[tag];
         order.mOrderState = OrderState::Inserted;
         // TODO other slow thread things here (i.e. push into slow thread message queue)
-    });
-    // TODO pubsub out a message that we have an order insert, but do this on the slow thread
+    }, fillCallback);
+    // TODO pubsub out a message that we have an order insert, but do this on the slow thread, by passing a message to slow thread
   }
 
 public:
@@ -126,13 +228,12 @@ public:
     mEventEngine(eventEngine),
     mExecModule(mEventEngine)
   {
-    // TODO create fifo pipe here
-    mEventEngine.RegisterPipeListener("OrderManager", *this);
+    mEventEngine.RegisterPipeListener("/tmp/OrderManager", *this);
   }
 
   void OnData(const char* data, size_t msgLen) override
   {
-    // event engine thread
+    // assume that this is an order insert message
     HandleInsertOrder(OrderInsertMsg());
   }
 };
@@ -156,5 +257,8 @@ int main()
   eventEngine.Start();
 
   // TODO have a second thread which does the slow work (writes to output pipe, etc), including a single writer, single reader message queue
+  // TODO handle price collars - write to ping pong buffer, and then inspect (generate randomly from slow thread)
+  // TODO replace std functions with lambdas
+  // TODO implement own callable queue?
 }
 
